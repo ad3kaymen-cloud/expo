ac5be9d6f0df4894281bc953ba10376e2d201f5d
Wiktor Smaga <wiktor.smaga13@gmail.com>
2026-06-22 17:41:43 +0200

    [cellular][Android] Remove legacy networkType path and suppress warnings

--- packages/expo-cellular/android/src/main/java/expo/modules/cellular/CellularModule.kt
+++ packages/expo-cellular/android/src/main/java/expo/modules/cellular/CellularModule.kt
@@ -4,7 +4,6 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
 import android.net.sip.SipManager
-import android.os.Build
 import android.telephony.TelephonyManager
 import android.util.Log
 import expo.modules.interfaces.permissions.Permissions
@@ -77,16 +76,11 @@ class CellularModule : Module() {
     get() = appContext.permissions ?: throw Exceptions.PermissionsModuleNotFound()
 
   @SuppressLint("MissingPermission")
+  @Suppress("DEPRECATION") // Legacy CDMA constants are deprecated, but can still be returned on older devices, so we keep mapping them instead of returning UNKNOWN
   private fun getCurrentGeneration(): Int {
     val telephonyManager = telephonyManager()
       ?: return CellularGeneration.UNKNOWN.value
-    val networkType = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-      telephonyManager.dataNetworkType
-    } else {
-      @Suppress("DEPRECATION")
-      telephonyManager.networkType
-    }
-    return when (networkType) {
+    return when (telephonyManager.dataNetworkType) {
       TelephonyManager.NETWORK_TYPE_GPRS,
       TelephonyManager.NETWORK_TYPE_EDGE,
       TelephonyManager.NETWORK_TYPE_CDMA,
